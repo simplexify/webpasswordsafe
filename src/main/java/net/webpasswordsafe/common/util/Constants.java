91077d963e542cfe875dbd78b75da749644513d3
Glenn Provoost <glenn@ccie.be>
2016-10-07 17:47:05 +0200

    Update Constants.java

--- src/main/java/net/webpasswordsafe/common/util/Constants.java
+++ src/main/java/net/webpasswordsafe/common/util/Constants.java
@@ -29,7 +29,7 @@ package net.webpasswordsafe.common.util;
 public class Constants
 {
     // versioning
-    public static final String VERSION = "1.4";
+    public static final String VERSION = "1.4.1";
     public static final String COPYRIGHT = "2008-2015 Josh Drummond";
     public static final String URL_WEBPASSWORDSAFE = "http://www.webpasswordsafe.net";
     public static final String URL_HELP =  "docs/UserGuide.html";
